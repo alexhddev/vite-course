2f10931ecc85c25042b20790b736c7f8e922b6ad
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-06-06 18:32:19 +0300

    multiple routes

--- simple-js/vite.config.js
+++ simple-js/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     build: {
-        minify: false
+        minify: false,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                admin: resolve(__dirname, 'admin/index.html'),
+            }
+        }
     },
     envPrefix: 'COOL_APP_'
 })
