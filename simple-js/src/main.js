f9c0cd8ec9d02ac6d34d169189fdc3d4584f17b1
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-06-10 12:18:06 +0300

    plugins

--- simple-js/src/main.js
+++ simple-js/src/main.js
@@ -34,4 +34,4 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
-console.log(import.meta.env)
+console.log('Hello from Vite!!!!')
--- simple-js/vite.config.js
+++ simple-js/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import removeConsole from "vite-plugin-remove-console";
 
 export default defineConfig({
+    plugins:[removeConsole()],
     build: {
         minify: false,
         rollupOptions: {
